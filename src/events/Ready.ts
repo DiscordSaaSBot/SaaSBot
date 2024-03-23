4bfdaa259cac8034c35be982eb21ecf16cf08ff8
Esteve Autet Alexe <stifskere@gmail.com>
2024-03-23 12:48:37 +0100

    feat: status change

--- src/events/Ready.ts
+++ src/events/Ready.ts
@@ -1,11 +1,28 @@
 import {Event} from "../modules/handlers/HandlerBuilders.js";
-import {Events} from "discord.js";
+import {ActivityOptions, ActivityType, Events} from "discord.js";
 import {logger} from "../modules/utils/logger.js";
+import {EventContext} from "../modules/handlers/HandlerContext.js";
+
+const presences: ActivityOptions[] = [
+	{ name: "your SaaS projects", type: ActivityType.Watching },
+	{ name: "for your contributions", type: ActivityType.Listening },
+	{ name: "with your SaaS project", type: ActivityType.Playing }
+];
 
 export default new Event({
 	event: Events.ClientReady,
 
 	handler(): void {
+		let currentStatus: number = 0;
+		function changeStatus(this: EventContext): void {
+			this.client.user!.setActivity(
+presences[++currentStatus === presences.length ? 0 : currentStatus]
+			);
+		}
+
+		setInterval(changeStatus.bind(this), 3600000 * 5);
+		changeStatus.call(this);
+
 		logger.info("Client ready!");
 	}
 });
