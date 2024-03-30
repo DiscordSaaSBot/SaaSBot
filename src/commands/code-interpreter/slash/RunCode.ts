77b4817bc894c10e1b5b8692b84b00c0f1cf0377
TheMineWay <joelcamposoliva9000@gmail.com>
2024-03-30 09:34:55 +0100

    feat(imports): use path aliases everywhere

--- src/commands/code-interpreter/slash/RunCode.ts
+++ src/commands/code-interpreter/slash/RunCode.ts
@@ -1,4 +1,4 @@
-import { SlashCommand } from "../../../modules/handlers/HandlerBuilders.js";
+import { SlashCommand } from "@modules/handlers/HandlerBuilders.js";
 import { Attachment, EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import { CreateRunnerResponse, GetRunnerDetails } from "../utils/ApiTypes.js";
 import {
